2901ebb357aae2ced5ae077e4a20b201759d8b41
Akash-Bharathi <113248732+Akash-Bharathi@users.noreply.github.com>
2024-07-19 20:31:12 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+<link href='../www.blogger.com/static/v1/widgets/2975350028-css_bundle_v2.css' rel='stylesheet' type='text/css'/>
+<meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+<link href="../fonts.googleapis.com/css6079.css?family=Poppins:300,400,500,600,700" rel="stylesheet" type="text/css">
+<link href='../maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css' rel='stylesheet'/>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+<meta content='blogger' name='generator'/>
+<link href='favicon.ico' rel='icon' type='image/x-icon'/>
+<link href='index.html' rel='canonical'/>
+<link rel="alternate" type="application/atom+xml" title="Sports Mag - Atom" href="feeds/posts/default" />
+<link rel="alternate" type="application/rss+xml" title="Sports Mag - RSS" href="feeds/posts/default9522?alt=rss" />
+<link rel="service.post" type="application/atom+xml" title="Sports Mag - Atom" href="../www.blogger.com/feeds/4405054252348132187/posts/default" />
+<link rel="me" href="../www.blogger.com/profile/13466932854452109456.html" />
+<!--[if IE]><script type="text/javascript" src="https://www.blogger.com/static/v1/jsbin/2068738220-ieretrofit.js"></script>
+<![endif]-->
+<meta content='http://sports- .blogspot.com/' property='og:url'/>
+<meta content='Sports Mag' property='og:title'/>
+<meta content='' property='og:description'/>
+<!--[if IE]> <script> (function() { var html5 = ("abbr,article,aside,audio,canvas,datalist,details," + "figure,footer,header,hgroup,mark,menu,meter,nav,output," + "progress,section,time,video").split(','); for (var i = 0; i < html5.length; i++) { document.createElement(html5[i]); } try { document.execCommand('BackgroundImageCache', false, true); } catch(e) {} })(); </script> <![endif]-->
+<title>
+College Blog
+</title>
+<!-- Description and Keywords (start) -->
+<meta content='YOUR DESCRIPTION HERE' name='description'/>
+<meta content='YOUR KEYWORDS HERE' name='keywords'/>
+<!-- Description and Keywords (end) -->
+<meta content='Sports Mag' property='og:site_name'/>
+<meta content='https://sports- .blogspot.com/' name='twitter:domain'/>
+<meta content='' name='twitter:title'/>
+<meta content='summary' name='twitter:card'/>
+<meta content='' name='twitter:title'/>
+<!-- Social Media meta tag need customer customization -->
+<meta content='Facebook App ID here' property='fb:app_id'/>
+<meta content='Facebook Admin ID here' property='fb:admins'/>
+<meta content='@username' name='twitter:site'/>
+<meta content='@username' name='twitter:creator'/>
---
+++ menu.php
@@ -0,0 +1,38 @@
+
+<style id='page-skin-1' type='text/css'>
+<?php include 'style1.css';?>
+</div></div>
+</div>
+</div>
+
+<div id='top'></div>
+<div id='topnav'>
+<div class='tm-head row'>
+<div class='tm-menu'>
+<div class='menu1 section' id='menu1' name='Top Navigation'><div class='widget LinkList' data-version='1' id='LinkList210'>
+<div class='widget-content'>
+<ul id='nav1'>
+<li><a href='index.php'>home</a></li>
+<li><a href='dashboard/index3164.html'>Profile</a></li>
+<li><a href='text_editor/index.html'>Create</a></li>
+<li><a href='login.php'>Login</a></li>
+</ul>
+</div>
+</div></div>
+</div>
+<!-- Top Social -->
+<div class='social-area'>
+<div class='social-top section' id='social-top' name='Social Top'><div class='widget LinkList' data-version='1' id='LinkList50'>
+<div class='widget-content'>
+<ul>
+<li><a class='facebook' href='../facebook.com/index.html' target='_blank' title='facebook'></a></li>
+<li><a class='twitter' href='../twitter.com/index.html' target='_blank' title='twitter'></a></li>
+<li><a class='instagram' href='../instagram.com/index.html' target='_blank' title='instagram'></a></li>
+<li><a class='pinterest' href='../pinterest.com/index.html' target='_blank' title='pinterest'></a></li>
+<li><a class='gplus' href='../plus.google.com/index.html' target='_blank' title='gplus'></a></li>
+</ul>
+</div>
+</div></div>
+</div>
+</div>
+</div>
